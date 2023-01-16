a7b8c577d48460ddc97cc3e86a760886021aa14d
gusdas <gustmd213@naver.com>
2023-01-16 21:41:49 +0900

    practice 2

--- js/1.js
+++ js/1.js
@@ -3,14 +3,10 @@ function previewFile() {
   const file = document.querySelector("#imageSelector").files[0];
   const reader = new FileReader();
 
-  reader.addEventListener(
-    "load",
-    () => {
-      // convert image file to base64 string
-      preview.src = reader.result;
-    },
-    false
-  );
+  reader.addEventListener("load", () => {
+    // convert image file to base64 string
+    preview.src = reader.result;
+  });
 
   if (file) {
     reader.readAsDataURL(file);
@@ -24,3 +20,33 @@ function show() {
 function popupClose() {
   document.querySelector(".background").className = "background";
 }
+
+//1. 쿼리셀렉터
+let a = document.querySelectorAll("P.test")[1].innerHTML;
+console.log(a);
+
+let b = document
+  .querySelectorAll("ul.testUl")[0]
+  .querySelectorAll("li")[1].innerText;
+console.log(b);
+
+let c = document
+  .querySelectorAll("ul.testUl")[1]
+  .querySelectorAll("li")[2].innerText;
+console.log(c);
+
+//4. API 객체 컨트롤
+
+const API = [
+  { userId: 1, id: 1, title: "밥 먹기", completed: false },
+  { userId: 1, id: 2, title: "공부 하기", completed: false },
+  { userId: 1, id: 3, title: "커피 마시기", completed: true },
+  { userId: 1, id: 4, title: "씻기", completed: true },
+  { userId: 1, id: 5, title: "잠 자기", completed: false },
+];
+
+const setAPIElements = () => {
+  const objectParent = document.querySelector("#objectParent");
+
+  objectParent.innerHTML = console.log();
+};
