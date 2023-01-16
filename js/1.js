fe11feaed9eb4d9e9ebebcc7cfa92042b8fa65c3
gusdas <gustmd213@naver.com>
2023-01-16 22:25:05 +0900

    prettier setting

--- js/1.js
+++ js/1.js
@@ -1,9 +1,9 @@
 function previewFile() {
-  const preview = document.querySelector("#previewImage");
-  const file = document.querySelector("#imageSelector").files[0];
+  const preview = document.querySelector('#previewImage');
+  const file = document.querySelector('#imageSelector').files[0];
   const reader = new FileReader();
 
-  reader.addEventListener("load", () => {
+  reader.addEventListener('load', () => {
     // convert image file to base64 string
     preview.src = reader.result;
   });
@@ -14,11 +14,11 @@ function previewFile() {
 }
 
 function show() {
-  document.querySelector(".background").className = "background show";
+  document.querySelector('.background').className = 'background show';
 }
 
 function popupClose() {
-  document.querySelector(".background").className = "background";
+  document.querySelector('.background').className = 'background';
 }
 
 const create2DArray = (rows, columns) => {
@@ -30,15 +30,15 @@ const create2DArray = (rows, columns) => {
 };
 
 const makeElement = () => {
-  const ulParent = document.getElementById("ulParent");
+  const ulParent = document.getElementById('ulParent');
   let arr = create2DArray(3, 4);
   arr.map((row, rowIdx) => {
-    const newUL = document.createElement("ul");
+    const newUL = document.createElement('ul');
     // newUL.setAttribute('id', 'ul' + rowIdx);
     newUL.innerText = rowIdx;
     ulParent.appendChild(newUL);
     row.map((col, colIdx) => {
-      const newli = document.createElement("li");
+      const newli = document.createElement('li');
       newli.innerText = col;
       // const liParent = document.getElementById('ul' + rowIdx);
       // liParent.appendChild(newli);
@@ -48,37 +48,36 @@ const makeElement = () => {
 };
 
 //1. 쿼리셀렉터
-let a = document.querySelectorAll("P.test")[1].innerHTML;
+let a = document.querySelectorAll('P.test')[1].innerHTML;
 console.log(a);
 
 let b = document
-  .querySelectorAll("ul.testUl")[0]
-  .querySelectorAll("li")[1].innerText;
+  .querySelectorAll('ul.testUl')[0]
+  .querySelectorAll('li')[1].innerText;
 console.log(b);
 
 let c = document
-  .querySelectorAll("ul.testUl")[1]
-  .querySelectorAll("li")[2].innerText;
+  .querySelectorAll('ul.testUl')[1]
+  .querySelectorAll('li')[2].innerText;
 console.log(c);
 
 //4. API 객체 컨트롤
-
 const API = [
-  { userId: 1, id: 1, title: "밥 먹기", completed: false },
-  { userId: 1, id: 2, title: "공부 하기", completed: false },
-  { userId: 1, id: 3, title: "커피 마시기", completed: true },
-  { userId: 1, id: 4, title: "씻기", completed: true },
-  { userId: 1, id: 5, title: "잠 자기", completed: false },
+  { userId: 1, id: 1, title: '밥 먹기', completed: false },
+  { userId: 1, id: 2, title: '공부 하기', completed: false },
+  { userId: 1, id: 3, title: '커피 마시기', completed: true },
+  { userId: 1, id: 4, title: '씻기', completed: true },
+  { userId: 1, id: 5, title: '잠 자기', completed: false },
 ];
 
 const setAPIElements = () => {
   API.map((parent, rowIdx) => {
-    const newUl = document.createElement("ul");
+    const newUl = document.createElement('ul');
     newUl.innerText = `id : ${parent.id}`;
     objectParent.appendChild(newUl);
     Object.entries(parent).map(([key, value]) => {
-      if (key != "id") {
-        const newLi = document.createElement("li");
+      if (key != 'id') {
+        const newLi = document.createElement('li');
         newLi.innerText = `${key}: ${value}`;
         newUl.appendChild(newLi);
       }
